b1d117f54f321e4dfa165555581b80ecfc5d2d6d
julydeath <manojkarajada.mk@gmail.com>
2023-05-24 14:09:24 +0530

    chore : bcrypt added

--- routers/auth.js
+++ routers/auth.js
@@ -1,14 +1,16 @@
 const route = require("express").Router();
 const userAuth = require("../model/User.js");
+const bcrypt = require("bcrypt");
 
 route.post("/register", async (req, res) => {
   try {
+    const salt = await bcrypt.genSalt();
     const isUser = await userAuth.findOne({ email: req.body.email });
     if (!isUser) {
       const newUser = new userAuth({
         email: req.body.email,
         phone: req.body.phone,
-        password: req.body.password,
+        password: await bcrypt.hash(req.body.password, salt),
       });
       await newUser.save();
       res.status(200).json("user registered succussfully");
@@ -24,7 +26,7 @@ route.post("/login", async (req, res) => {
   try {
     const isUser = await userAuth.findOne({ email: req.body.email });
     if (isUser) {
-      if (isUser.password === req.body.password) {
+      if (await bcrypt.compare(req.body.password, isUser.password)) {
         res.status(200).json("login success");
       } else {
         res.status(409).json("wrong password");
